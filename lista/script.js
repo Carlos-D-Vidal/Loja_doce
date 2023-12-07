b83e5fb0800f4bb210881f6f12bf801844aadd1c
Carlos-D-Vidal <carlosvidal3d@outlook.com>
2023-12-07 19:46:15 -0300

    mensagem

---
+++ lista/script.js
@@ -0,0 +1,26 @@
+import {menu} from '../menu.js'
+const myHeaders = {
+    "Content-Type": "application/json",
+};
+async function lista() {
+    const res = await fetch(
+        "http://localhost:3001/produtos"
+    )
+    const prod = await res.json()
+    console.log(prod)
+    const ul = document.querySelector("ul")
+    ul.innerHTML = ""
+    prod.forEach((produtos) => {
+        ul.insertAdjacentHTML("beforeend", `
+            <li id="lista-li">
+                <p>Nome: ${produtos.nome}</p>
+                <p>Custo: $ ${parseFloat(produtos.custo).toFixed(2)}</p>
+                <p>Venda: $ ${parseFloat(produtos.venda).toFixed(2)}</p>
+            </li>
+        `)
+    })
+}
+lista()
+
+const header = document.querySelector("header")
+header.innerHTML = menu
---
+++ menu.js
@@ -0,0 +1,7 @@
+export const menu =`<nav id="menu">
+<div>
+    <a href="/home/index.html">Cadastro de produtos</a>
+    <a href="/lista/index.html">Lista produtos</a>
+    <a href="/venda/index.html">Venda</a>
+</div>
+</nav>`
