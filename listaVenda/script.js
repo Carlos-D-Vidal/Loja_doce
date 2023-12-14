cdcf4dfae0e2625e4c1dbbf46c63848ad0cb9561
Carlos-D-Vidal <144069356+Carlos-D-Vidal@users.noreply.github.com>
2023-12-13 21:34:27 -0300

    Add files via upload

---
+++ listaVenda/script.js
@@ -0,0 +1,31 @@
+import { baseUrl } from "../api.js";
+import { menu } from "../menu.js";
+
+
+async function listaVenda() {
+    const res = await fetch(
+        `${baseUrl}/venda`
+    )
+
+const produtos = await fetch(`${baseUrl}/produtos`)
+const produtosJson = await produtos.json()
+const lista = await res.json()
+const ul = document.querySelector("ul")
+const produto = {
+    nome: produtosJson.nome,
+};
+const string = JSON.stringify(produto)
+ul.innerHTML = ""
+lista.forEach((venda) => {
+    ul.insertAdjacentHTML("beforeend", `
+    <li id="lista-li">
+        <p>Nome: ${string}</p>
+        <p>Valor da venda: ${venda.valorVenda}</p>
+        <p>Data da venda: ${venda.dia}/${venda.mês}/${venda.ano}</p>
+    </li>
+`)
+})
+}
+const header = document.querySelector("header")
+header.innerHTML = menu
+listaVenda()
--- menu.js
+++ menu.js
@@ -3,5 +3,6 @@ export const menu =`<nav id="menu">
     <a href="/home/index.html">Cadastro de produtos</a>
     <a href="/lista/index.html">Lista produtos</a>
     <a href="/venda/index.html">Venda</a>
+    <a href="/listaVenda/index.html">Lista de vendas</a>
 </div>
 </nav>`
